b24c19090640d0fef17a29736aa94e94d10bf850
苏斌 <binsu.cs@gmail.com>
2018-03-14 21:07:15 +0800

    WavCut1.0

--- WavCut.py
+++ WavCut.py
@@ -1,4 +1,18 @@
-# 支持mp3
+''' 
+支持wav文件
+该脚本可以从一段wav文件中以秒单位截取一部分音频，默认截取120秒，如果音乐不足120秒
+则全部保留。
+
+基本用法如下：
+WavCut inputWav outputWav
+inputWav是源文件，outputWav是输出文件，如果该文件存在，则会被覆盖。
+
+此外可以通过-b参数指定要开始截取的位置(以秒计算，必须为整数)，-e参数可以指定截取的
+结束位置(以秒计算，必须为整数)。若仅指定-b则截取[begin_time,begin_time+120)区间内
+的音频，若仅指定-e则截取[0,end_time)区间的内容。如果end_time超出音频总时间，则只截
+取到音频末尾，不会补充空白。
+
+'''
 
 import sys,getopt
 import wave
@@ -15,37 +29,39 @@ def main(in_name,out_name,begin_time,end_time):
 
 	if begin_time>=end_time:
 		sys.stderr.write("begin_time must less than end_time")
-		return -2
+		return -1
 
 	try:
 		in_file=wave.open(in_name,'rb')
 	except:
 		sys.stderr.write("no such input file")
-		return -3
+		return -2
 	finally:
 		pass
 
 	params=in_file.getparams()
 
-	global simple_rate=params.framerate
+	global simple_rate
+	simple_rate=params.framerate
 
 	begin_frame=sec2frame(begin_time)
-	end_frame=sec2frame(end_frame)
+	end_frame=sec2frame(end_time)
 
-	if begin_time>info.frame_count:
+	if begin_frame>params.nframes:
 		sys.stderr.write("the begin time beyond the total length of wav")
-		return -4
+		return -3
 
 	in_file.setpos(begin_frame)
 	data=in_file.readframes(end_frame-begin_frame)
 
-	params.nframes=end_frame-begin_frame
+	params._replace(nframes=end_frame-begin_frame)
 	out_file=wave.open(out_name,'wb')
 	out_file.setparams(params)
 	out_file.writeframes(data)
 
 	in_file.close()
 	out_file.close()
+	return 0
 
 if __name__ == '__main__':
 
@@ -53,24 +69,26 @@ if __name__ == '__main__':
 	end_time=120
 	
 	try:
-		opts,args=getopt.getopt(sys.argv,"b:e:")
+		opts,args=getopt.getopt(sys.argv[1:],"b:e:",[])
 	except:
-		print("WavCut -i inputWav -o outputWav")
+		sys.stderr.write("can not parse the params")
+		exit(0)
 	finally:
 		pass
 
-	if len(args)!=3:
-		print("WavCut -i inputWav -o outputWav")
-		return -1
+	if len(args)!=2:
+		print("usage: WavCut inputWav outputWav")
+		exit(0)
 	else:
-		in_name=args[1]
-		out_name=args[2]
+		in_name=args[0]
+		out_name=args[1]
 
 	for opt,val in opts:
 		if opt == '-b':
-			begin_time=val
-			end_time=val+120
+			begin_time=int(val)
+			end_time=begin_time+120
 		elif opt=='-e':
-			end_time=val
+			end_time=int(val)
 
-	return main(in_name,out_name,begin_time,end_time)
+	statu=main(in_name,out_name,begin_time,end_time)
+	exit(statu)
